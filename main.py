6b610125a632953c99e7e8502344a7d649d875d5
AndrewWritesCode <47736760+AndrewWritesCode@users.noreply.github.com>
2022-04-22 22:47:41 -0700

    Fixed looping bug, need to add emal alerts, max file size / auto-deletion of old images

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ import datetime
 
 debug = False
 
+fps = 1.5 #Accepts a float
+cameraPort = 0
+cameraMode = ""
+maxSentryStorage = 1000 * pow(10,3) #[in MB] (onces this is reached oldest images will be deleted)
+fileUploadNotificationFreq = 0 #[int] Every 1/x file that is uploaded will be announce on terminal. 0 diables announcements
+if fileUploadNotificationFreq < 1:
+    fileUploadNotificationFreq = pow(10,10)
 #Calculates the disk space taken from a given FPS value and file size
 def DailySpaceConsumption(singleFileSizeEstimate, fps):
     dailySpaceCons = singleFileSizeEstimate*86400*fps
@@ -25,12 +32,12 @@ def FPS_step(fps):
     return fps_step
 
 #Sets/Creates the directory
-def DirectorySetup():
+def DirectorySetup(cameraMode):
     currentDate = datetime.datetime.now()
     dateString = str(currentDate.month) + 'm' + str(currentDate.day) + 'd' + str(currentDate.year) + 'y'
-    cameraMode = ""
     collection_type = ""
     collection_dir = ''
+    sentryStorage = 0.0
     while cameraMode != "T" or "S":
         mode = input("TRAINING OR SENTRY [T/S]: ")
         mode = mode.upper()
@@ -39,12 +46,13 @@ def DirectorySetup():
             collection_dir = 'doorSentry'
             break
         elif mode == "T" :
-            while collection_type != "L" or "U" or "S":
+            while collection_type != "L" or "U":
                 collection_type = input("TRAINING TYPE: LOCKED OR UNLOCKED? [L/U]: ")
                 collection_type = collection_type.upper()
                 if collection_type == "L":
                     collection_dir = 'locked_uncleaned'
                     print("Initializing Locked State Image Collection...")
+                    break
                     if debug:
                         CollectionDebug(collection_type)
                     break
@@ -54,7 +62,10 @@ def DirectorySetup():
                     if debug:
                         CollectionDebug(collection_type)
                     break
-                break
+        if collection_type == "L" or "U" or "S":
+            break
+
+
 
     try:
         os.mkdir('data')
@@ -70,31 +81,40 @@ def DirectorySetup():
 
     try:
         folderNum = len(os.listdir(os.getcwd())) + 1
+        if collection_type == "S":
+            for path, dirs, files in os.walk(os.getcwd()):
+                for file in files:
+                    filepath = os.path.join(path, file)
+                    sentryStorage = sentryStorage + os.path.getsize(filepath)
+                  #This calculcates the disk space given in bytes of the images already stored sentry storage
         os.mkdir(dateString)
+
     except:
         folderNum = len(os.listdir(os.getcwd())) + 1
         logging.debug('Using existing dateString directory')
     os.chdir(dateString)
 
-    return collection_type, folderNum
+    return collection_type, folderNum, sentryStorage
 
 
 ######################################
 
 
-collection_type, folderNum = DirectorySetup()
+collection_type, folderNum, sentryStorage = DirectorySetup(cameraMode)
+if collection_type == "S":
+    print(str(sentryStorage / pow(10,6)) + "MB of images are already saved to Sentry Storage...")
 print("Saving images to " + str(os.getcwd()))
 #The number of bytes that have been saved in the current session
-session_size = 0
+daily_session_size = 0
+
 for file in os.listdir(os.getcwd()):
-    session_size = session_size + os.path.getsize(file)
+    daily_session_size = daily_session_size + os.path.getsize(file)
     #This sums the file sizes in the current day's directory
-print("Session size intializing at " + str(session_size / pow(10,6)) + "MB")
+print("Session size intializing at " + str(daily_session_size / pow(10, 6)) + "MB")
 time_lastCapture = time.time()
 #initializes the time for fps calcs
-fps = 1.5 #Accepts a float
 fps_step = FPS_step(fps)
-cameraPort = 0
+
 try:
     cap = cv2.VideoCapture(cameraPort, cv2.CAP_DSHOW) #takes the first avaialble camera on computer
 except:
@@ -125,12 +145,15 @@ while (cap.isOpened()):
         filename = collection_type + '_ses' + str(folderNum) + '_num_' + str(fileNum) + '-' + str(fps) + 'FPS_' + \
                    dateString + '-' + timeString +  '.jpeg'
         cv2.imwrite(filename, frame)
-        session_size = session_size + os.path.getsize(filename)
-        print('Saving ' + str(filename) + ', Total Session Size = ' + str(session_size/pow(10,6)) + 'MB')
+        daily_session_size = daily_session_size + os.path.getsize(filename)
+        sentryStorage = sentryStorage + os.path.getsize(filename)
+        if fileNum % fileUploadNotificationFreq == 0:
+            print('Saving ' + str(filename) + ', Daily Session Size = ' + str(daily_session_size / pow(10, 6)) + \
+                  'MB, All Sessions Size = ' + str(sentryStorage / pow(10,6)) + 'MB, TO END: press \'q\' on VideoStream')
         time_lastCapture = time_now
     #Checks to see if the day the frame was taken matches the day the session began
     if currentDate.day - startDay != 0:
-        print("Day complete, saved " + str(session_size) + "MB for day, moving to new directory...")
+        print("Day complete, saved " + str(daily_session_size) + "MB for day, moving to new directory...")
         os.chdir('..')
         try:
             dateString = str(currentDate.month) + 'm' + str(currentDate.day) + 'd' + str(currentDate.year) + 'y'
@@ -140,6 +163,7 @@ while (cap.isOpened()):
             os.chdir(dateString)
             print("Now saving images to " + str(os.getcwd()))
             startDay = currentDate.day #updates the day the sessions began
+            daily_session_size = 0.0
         except:
             logging.WARN("Unable to create directory for next day")
 
